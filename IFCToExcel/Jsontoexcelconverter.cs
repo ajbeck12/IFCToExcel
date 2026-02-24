456be2fb4e43d3c678cf4c6727a832a867c5c565
Austin Beck <abeck@wildeck.com>
2026-02-24 14:24:38 -0600

    1

--- IFCToExcel/Jsontoexcelconverter.cs
+++ IFCToExcel/Jsontoexcelconverter.cs
@@ -300,7 +300,9 @@ namespace IfcToExcelWinForms
             }
             return result;
         }
-        private static string ClassifyFaceFromPlate(JsonElement plateElem)
+
+        private
+private static string ClassifyFaceFromPlate(JsonElement plateElem)
         {
             // Prefer plate normal (axisZ) if present; it's robust even when connection point isn't centered.
             if (plateElem.ValueKind == JsonValueKind.Object &&
